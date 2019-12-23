709b264eb9f5d8526b5819e0d672572a8919f3a8
Igor Sidnev <mr.igor.sidnev@gmail.com>
2019-12-23 14:59:00 +0000

    Adding comments about logging errors

--- startup/config.js
+++ startup/config.js
@@ -3,5 +3,6 @@ const config = require('config');
 module.exports = function() {
   if (!config.get('jwtPrivateKey')) {
     throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+    //after the error is thrown the process will be automatically terminated
   }
 }
--- startup/logging.js
+++ startup/logging.js
@@ -4,12 +4,13 @@ require('express-async-errors');
 
 module.exports = function() {
   winston.handleExceptions(
+    /*the next line displays errors on the console*/
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
 
   process.on('unhandledRejection', (ex) => {
     throw ex;
-  });
+  }); //this line catches all the errors related to Promises/async operations (rejected promises)
 
   winston.add(winston.transports.File, { filename: 'logfile.log' }); //we write error in the log file
   winston.add(winston.transports.MongoDB, {
