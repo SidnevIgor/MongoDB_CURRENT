0429a021dc471166bc0568f6db9d8e425cdad98e
Igor Sidnev <mr.igor.sidnev@gmail.com>
2019-12-24 12:32:02 +0000

    Adding unit test for JSON token creation

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -3,11 +3,11 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const mongoose = require('mongoose');
 
-describe('user.generateAuthToken', () => {
-  it('should return a valid JWT', () => {
-    const payload = { 
-      _id: new mongoose.Types.ObjectId().toHexString(), 
-      isAdmin: true 
+describe('user.generateAuthToken', function(){
+  test('should return a valid JWT', function(){
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
     };
     const user = new User(payload);
     const token = user.generateAuthToken();
