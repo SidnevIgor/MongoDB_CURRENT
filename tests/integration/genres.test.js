9018f1a179a7dc3669c84754310a56cd146c8488
Igor Sidnev <mr.igor.sidnev@gmail.com>
2019-12-24 16:54:47 +0000

    Adding test for the overall number of genres and checking the existance of one specific genre

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest'); //here we add a linrary to build http requests
+const {Genre} = require('../../models/genre');
 let server;
 
 describe('api/genres', function(){
@@ -6,13 +7,20 @@ describe('api/genres', function(){
     server = require('../../index.js');
     console.log(server);
   });
-  afterEach(function(){
+  afterEach(async function(){
     server.close();
+    await Genre.remove({});
   });
   describe('GET', function(){
     it('Should return all genres', async function(){
+      await Genre.collection.insertMany([
+        {name:"romance"},
+        {name:"horror"}
+      ]);
       const res = await request(server).get('/api/genres'); //we replace the request normally done through Postman
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some(val=>val.name==="romance")).toBeTruthy();
     });
   });
 });
