3c7d8984ecd5a11bf0a78b11d54563fa8c036f16
Igor Sidnev <mr.igor.sidnev@gmail.com>
2020-01-08 18:22:04 +0000

    Deleting unnecessary data from form object

--- front/src/app/contact-form/contact-form.component.ts
+++ front/src/app/contact-form/contact-form.component.ts
@@ -6,11 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent{
-  contactMethods = [
-    {id:1, name:"Email"},
-    {id:2, name:"Phone"},
-    {id:3, name:"Viber"}
-  ];
   submit(f){
     console.log(f);
   }
