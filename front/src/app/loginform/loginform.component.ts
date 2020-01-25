dce9772a8c547e7caee5aac4f4091f5531e60f21
Igor Sidnev <mr.igor.sidnev@gmail.com>
2020-01-25 18:50:36 +0000

    Validating bad server responce with login and signup forms

--- front/src/app/loginform/loginform.component.ts
+++ front/src/app/loginform/loginform.component.ts
@@ -1,6 +1,11 @@
 import { Component} from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+//POST service
 import { PostService } from '../services/post.service';
+//error services
+import { AppError } from '../common/app-error';
+import { BadRequestError } from '../common/bad-request-error';
+import { NotFoundError } from '../common/not-found-error';
 
 @Component({
   selector: 'loginform',
@@ -9,6 +14,7 @@ import { PostService } from '../services/post.service';
 })
 export class LoginformComponent{
   constructor(private service: PostService){}
+  errorMessage:string;
   url = "http://localhost:3000/api/auth";
   form = new FormGroup({
     email: new FormControl('',[
@@ -30,6 +36,15 @@ export class LoginformComponent{
     this.service.addPost(user,this.url)
     .subscribe((responce)=>{
       console.log(responce);
+      this.errorMessage = null;
+    },(error: AppError)=>{
+      if(error instanceof BadRequestError) this.errorMessage = "Invalid Email or Password";
+      else{
+        if(error instanceof NotFoundError) alert('Not found error');
+        else{
+          alert('Some error has happened');
+        }
+      }
     });
   }
 }
